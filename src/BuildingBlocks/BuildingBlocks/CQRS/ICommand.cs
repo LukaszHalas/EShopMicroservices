47fa911444ca0eca12a19bed61def7dc14adffdc
lukasz.halas <lukasz.halas95@gmail.com>
2025-05-14 09:43:55 +0200

    Section 5 completed

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System.Data.Common;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+}
+
+// Słowo kluczowe out w public interface ICommand<out TResponse> oznacza kowariancję –
+// czyli pozwala używać bardziej ogólnego typu zamiast dokładnego w kontekście typu wyjściowego
+// Umożliwia przypisanie Producer<Derived> do Producer<Base>
+
+// ICommand<Dog> dogCommand;
+// ICommand<Animal> animalCommand = dogCommand; // możliwe dzięki `out`
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandHandler<in TCommand>
+    : IRequestHandler<TCommand, Unit>
+    where TCommand : ICommand
+{
+}
+
+// Słowo kluczowe in w C# przy deklaracji typu generycznego oznacza kontrawariancję —
+// czyli że typ można zastąpić typem bazowym  w kontekście typy wejściowego
+// Umożliwia przypisanie Handler<Base> do Handler<Derived>
+
+// class MyHandler : ICommandHandler<BaseCommand, string> { }
+// ICommandHandler<DerivedCommand, string> handler = new MyHandler(); // OK — kontrawariancja
+
+// Jezeli nie ma slowka in oraz out to taki typ byłby traktowany jako inwariantny, czyli:
+// ICommandHandler<BaseCommand, TResponse> nie byłby kompatybilny z ICommandHandler<DerivedCommand, TResponse> ani odwrotnie.
+public interface ICommandHandler<in TCommand, TResponse> 
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
